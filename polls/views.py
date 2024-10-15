ebfdc5148901adbb1834b6bb9dc4896d8f0ee07d
wangyangang <wangyangang@gitcloud.com.cn>
2024-10-15 23:10:52 +0800

    first commit

---
+++ polls/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("对象不存在")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
